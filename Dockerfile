149e864f98b851baea280802bc0930a470da53e5
Michael Murray <ashesfall@almostrealism.com>
2022-09-04 17:04:37 -0700

    feat: extracted setup process into init.sh so that the login credentials can be configured at runtime.

--- Dockerfile
+++ Dockerfile
@@ -8,3 +8,7 @@ RUN pip install "PyAthena>1.2.0"
 
 # Switching back to using the `superset` user
 USER superset
+
+COPY scripts/init.sh /init.sh
+
+ENTRYPOINT /init.sh
--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 echo "Building container image..."
 docker build . -t ashesfall/analytics-portal:latest --network=host
 
-echo "Pushing container image..."
-docker push ashesfall/analytics-portal:latest
+# echo "Pushing container image..."
+# docker push ashesfall/analytics-portal:latest
