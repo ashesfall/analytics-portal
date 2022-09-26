3731a042bdf1975812c416106d257a669e0c3f5a
Michael Murray <ashesfall@almostrealism.com>
2022-09-26 11:27:46 -0700

    fix: accept entire s3 url for geojson; it does not have to be in the dashboards bucket.

--- scripts/dashboard_download.py
+++ scripts/dashboard_download.py
@@ -2,11 +2,11 @@ import os
 import awswrangler as wr
 
 dashboard_bucket = os.getenv("DASHBOARDS_BUCKET")
-geojson_file = os.getenv("GEOJSON_FILE")
+geojson_s3_url = os.getenv("GEOJSON_S3_URL")
 
 if __name__ == '__main__':
     print("Downloading geojson from S3...")
-    wr.s3.download(path=f's3://{dashboard_bucket}/{geojson_file}', local_file='/backups/data.geojson')
+    wr.s3.download(path=geojson_s3_url, local_file='/backups/data.geojson')
 
     print("Downloading dashboards from S3...")
     wr.s3.download(path=f's3://{dashboard_bucket}/dashboards.zip', local_file='/backups/dashboards.zip')
